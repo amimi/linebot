cba384b1b25f1e720839ce34f693d1c2742fbe2f
Ami <amimimimi@gmail.com>
2016-05-22 22:39:49 +0900

    index test

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,14 +4,14 @@ $jsonObj = json_decode($json_string);
 $to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
 
 // テキストで返事をする場合
-$response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"hello"];
+$response_format_text = array('contentType'=>1,"toType"=>1,"text"=>"hello");
 // 画像で返事をする場合
-$response_format_image = ['contentType'=>2,"toType"=>1,'originalContentUrl'=>"画像URL","previewImageUrl"=>"サムネイル画像URL"];
+$response_format_image = array('contentType'=>2,"toType"=>1,'originalContentUrl'=>"画像URL","previewImageUrl"=>"サムネイル画像URL");
 // 他にも色々ある
 // ....
 
 // toChannelとeventTypeは固定値なので、変更不要。
-$post_data = ["to"=>[$to],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_text];
+$post_data = array("to"=>[$to],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_text];
 
 $ch = curl_init("https://trialbot-api.line.me/v1/events");
 curl_setopt($ch, CURLOPT_POST, true);
