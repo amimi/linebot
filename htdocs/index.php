faa4d7eab18e448a35764f86840b5230dc1b690e
Ami <amimimimi@gmail.com>
2016-05-22 22:16:05 +0900

    index test

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,2 +1,28 @@
 <?php
-echo "Hellow linebot!";
+$json_string = file_get_contents('php://input');
+$jsonObj = json_decode($json_string);
+$to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
+
+// テキストで返事をする場合
+$response_format_text = ['contentType'=>1,"toType"=>1,"text"=>"hello"];
+// 画像で返事をする場合
+$response_format_image = ['contentType'=>2,"toType"=>1,'originalContentUrl'=>"画像URL","previewImageUrl"=>"サムネイル画像URL"];
+// 他にも色々ある
+// ....
+
+// toChannelとeventTypeは固定値なので、変更不要。
+$post_data = ["to"=>[$to],"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_text];
+
+$ch = curl_init("https://trialbot-api.line.me/v1/events");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charser=UTF-8',
+    'X-Line-ChannelID: 1462645442',
+    'X-Line-ChannelSecret: 1c8ed524537384435c2f3e9e93a872ab',
+    'X-Line-Trusted-User-With-ACL: u1ad1829c62e8b0d7fb8af24a3e1c4bdc'
+    ));
+$result = curl_exec($ch);
+curl_close($ch);
