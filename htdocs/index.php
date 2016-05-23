d62e6c77f9f6aaaec8a89b6130dc77402ab04eba
Ami <amimimimi@gmail.com>
2016-05-23 21:13:13 +0900

    index test

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,19 @@
 <?php
+
+const CHANNEL_ID = '1462645442';
+const CHANNEL_SECRET = '1c8ed524537384435c2f3e9e93a872ab';
+const MID = 'u1ad1829c62e8b0d7fb8af24a3e1c4bdc';
+
+// メッセージ受信
 $json_string = file_get_contents('php://input');
 $jsonObj = json_decode($json_string);
-$to = $jsonObj->{"result"}[0]->{"content"}->{"from"};
+$content = $json_object->result{0}->content;
+$text = $content->text;
+$from = $content->from;
+$message_id = $content->id;
+$content_type = $content->contentType;
+
+error_log($from);
 
 // テキストで返事をする場合
 $response_format_text = array('contentType'=>1,"toType"=>1,"text"=>"hello");
@@ -11,7 +23,12 @@ $response_format_image = array('contentType'=>2,"toType"=>1,'originalContentUrl'
 // ....
 
 // toChannelとeventTypeは固定値なので、変更不要。
-$post_data = array("to"=>array($to),"toChannel"=>"1383378250","eventType"=>"138311608800106203","content"=>$response_format_text);
+$post_data = [
+	"to"=>[$to],
+	"toChannel"=>"1383378250",
+	"eventType"=>"138311608800106203",
+	"content"=>$response_format_text
+];
 
 $ch = curl_init("https://trialbot-api.line.me/v1/events");
 curl_setopt($ch, CURLOPT_POST, true);
@@ -20,9 +37,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json; charser=UTF-8',
-    'X-Line-ChannelID: 1462645442',
-    'X-Line-ChannelSecret: 1c8ed524537384435c2f3e9e93a872ab',
-    'X-Line-Trusted-User-With-ACL: u1ad1829c62e8b0d7fb8af24a3e1c4bdc'
+    'X-Line-ChannelID: ' . CHANNEL_ID,
+    'X-Line-ChannelSecret: ' . CHANNEL_SECRET,
+    'X-Line-Trusted-User-With-ACL: ' . MID
     ));
 $result = curl_exec($ch);
 curl_close($ch);
