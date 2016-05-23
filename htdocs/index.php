d3a265106221d98e423472d95a57f997f71dd695
Ami <amimimimi@gmail.com>
2016-05-23 22:10:22 +0900

    index test

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,6 +11,8 @@ const CONTENT_TYPE_STAMP = 8;
 $hashi_mid = 'u7bf1339fb3b42acb906e5260b38cf53c';
 $hashi_icon = "http://dl.profile.line-cdn.net/0m0350b94372513e9e62c4fe9366de947d0e36e0d4e24e";
 
+$ami_mid = 'uba9d6e04158507756b57b4c3b952709e';
+
 // メッセージ受信
 $json_string = file_get_contents('php://input');
 $jsonObj = json_decode($json_string);
@@ -28,41 +30,49 @@ api_get_user_profile_request($from);
 if($content_type == CONTENT_TYPE_TEXT)
 {
 	// テキストを送ってきた場合
-	// テキストで返事をする場合
-	$response_format_text = [
-		'contentType'=> CONTENT_TYPE_TEXT,
-		"toType"=> 1,
-		"text"=> $text
-	];
+
+	if($text == 'はっしー')
+	{
+		// 画像で返事をする場合
+		$res_content = [
+			'contentType'=> CONTENT_TYPE_IMAGE,
+			"toType"=> 1,
+			'originalContentUrl'=>$hashi_icon,
+			"previewImageUrl"=> $hashi_icon
+		];	
+	}
+	else
+	{
+		// テキストでオウム返し
+		$res_content = [
+			'contentType'=> CONTENT_TYPE_TEXT,
+			"toType"=> 1,
+			"text"=> $text
+		];
+	}
 }
 else
 {
 	// テキスト以外を送ってきた場合
 	// テキストで返事をする場合
-	$response_format_text = [
+	$res_content = [
 		'contentType'=> CONTENT_TYPE_TEXT,
 		"toType"=> 1,
 		"text"=> 'なんなんだよ'
 	];
 }
 
-// 画像で返事をする場合
-$response_format_image = [
-	'contentType'=> CONTENT_TYPE_IMAGE,
-	"toType"=> 1,
-	'originalContentUrl'=>$hashi_icon,
-	"previewImageUrl"=> $hashi_icon
-];
+
 
 // 他にも色々ある
 // ....
 
 // toChannelとeventTypeは固定値なので、変更不要。
 $post_data = [
-	"to"=>[$from],
+	"to"=>[$ami_mid, $hashi_mid],
 	"toChannel"=>"1383378250",
 	"eventType"=>"138311608800106203",
-	"content"=>$response_format_image
+	"content"=>$res_content
 ];
 
 $ch = curl_init("https://trialbot-api.line.me/v1/events");
@@ -70,12 +80,12 @@ curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Content-Type: application/json; charser=UTF-8',
     'X-Line-ChannelID: ' . CHANNEL_ID,
     'X-Line-ChannelSecret: ' . CHANNEL_SECRET,
     'X-Line-Trusted-User-With-ACL: ' . MID
-    ));
+]);
 $result = curl_exec($ch);
 curl_close($ch);
 
