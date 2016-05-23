6c6c22a0dbaaba9829500f50d432b421ee31e324
Ami <amimimimi@gmail.com>
2016-05-23 21:48:11 +0900

    index test

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,6 +4,10 @@ const CHANNEL_ID = '1462645442';
 const CHANNEL_SECRET = '1c8ed524537384435c2f3e9e93a872ab';
 const MID = 'u1ad1829c62e8b0d7fb8af24a3e1c4bdc';
 
+const CONTENT_TYPE_TEXT = 1;
+const CONTENT_TYPE_IMAGE = 2;
+const CONTENT_TYPE_STAMP = 8;
+
 // メッセージ受信
 $json_string = file_get_contents('php://input');
 $jsonObj = json_decode($json_string);
@@ -15,16 +19,32 @@ $content_type = $content->contentType;
 
 error_log(print_r($content, TRUE));
 
-// テキストで返事をする場合
-$response_format_text = array('contentType'=>1,"toType"=>1,"text"=>"hello");
+// ユーザ情報取得
+api_get_user_profile_request($from);
+
+if($content_type == CONTENT_TYPE_TEXT)
+{
+	// テキストで返事をする場合
+	$response_format_text = [
+		'contentType'=> CONTENT_TYPE_TEXT,
+		"toType"=> 1,
+		"text"=> $text
+	];
+}
+
 // 画像で返事をする場合
-$response_format_image = array('contentType'=>2,"toType"=>1,'originalContentUrl'=>"画像URL","previewImageUrl"=>"サムネイル画像URL");
+$response_format_image = [
+	'contentType'=>2,
+	"toType"=>1,
+	'originalContentUrl'=>"画像URL",
+	"previewImageUrl"=>"サムネイル画像URL"
+];
 // 他にも色々ある
 // ....
 
 // toChannelとeventTypeは固定値なので、変更不要。
 $post_data = [
-	"to"=>[$to],
+	"to"=>[$from],
 	"toChannel"=>"1383378250",
 	"eventType"=>"138311608800106203",
 	"content"=>$response_format_text
@@ -43,3 +63,22 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     ));
 $result = curl_exec($ch);
 curl_close($ch);
+
+/**
+ * ユーザー情報取得
+ */
+function api_get_user_profile_request($mid) {
+    $url = "https://trialbot-api.line.me/v1/profiles?mids={$mid}";
+    $headers = array(
+        "X-Line-ChannelID: {CHANNEL_ID}",
+        "X-Line-ChannelSecret: {CHANNEL_SECRET}",
+        "X-Line-Trusted-User-With-ACL: {MID}"
+    ); 
+
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($curl);
+    error_log($output);
+}
+
