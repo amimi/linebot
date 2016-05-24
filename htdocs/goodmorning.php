a13ff1d73f38b7dd2e80f537035bbaa8c2f2ed98
Ami <amimimimi@gmail.com>
2016-05-25 00:59:14 +0900

    contact

--- htdocs/goodmorning.php
+++ htdocs/goodmorning.php
@@ -1,38 +1,31 @@
 <?php
-require_once(dirname(__FILE__) . '/../config.php');
+require_once(dirname(__FILE__) . '/Linebot.php');
 
-$hashi_mid = 'u7bf1339fb3b42acb906e5260b38cf53c';
+class Cron extends Linebot
+{
+	public function __construct()
+	{
+		$func = $argv[1];
+		if(function_exists($this->$func))
+		{
+			$this->$func();
+			error_log('実行しますよ：' + "{$this->$func}");
+		}
+		else
+		{
+			error_log('そんな関数ありませんよ' + "{$this->$func}");
+		}
+	}
+	
+	public function goodmorning()
+	{
+		$res_content = [
+			'contentType'=> CONTENT_TYPE_TEXT,
+			"toType"=> 1,
+			"text"=> 'おっはよーーー'
+		];
+		$this->send_message($this->ami_mid, $res_content);
+	}
+}
 
-$ami_mid = 'uba9d6e04158507756b57b4c3b952709e';
-
-// テキスト以外を送ってきた場合
-// テキストで返事をする場合
-$res_content = [
-	'contentType'=> CONTENT_TYPE_TEXT,
-	"toType"=> 1,
-	"text"=> 'おっはよーーー'
-];
-
-// toChannelとeventTypeは固定値なので、変更不要。
-$post_data = [
-	"to"=>[$ami_mid, $hashi_mid],
-	"toChannel"=>"1383378250",
-	"eventType"=>"138311608800106203",
-	"content"=>$res_content
-];
-
-$ch = curl_init("https://trialbot-api.line.me/v1/events");
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json; charser=UTF-8',
-    'X-Line-ChannelID: ' . CHANNEL_ID,
-    'X-Line-ChannelSecret: ' . CHANNEL_SECRET,
-    'X-Line-Trusted-User-With-ACL: ' . MID
-]);
-$result = curl_exec($ch);
-curl_close($ch);
-
-
+new Cron;
