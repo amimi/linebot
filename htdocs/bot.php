0f1f3466ee6d2fd69bc84489ccbc3cab35670d67
Ami <amimimimi@gmail.com>
2016-05-24 23:52:22 +0900

    contact

--- htdocs/bot.php
+++ htdocs/bot.php
@@ -1,16 +1,16 @@
 <?php
 require_once(dirname(__FILE__) . '/Linebot.php');
 
-class Bot extends Linebot {
-	
+class Bot extends Linebot
+{
 	public function __construct()
 	{
 		$res_content = [
 			'contentType'=> self::CONTENT_TYPE_STICKER,
-			"toType"=> 1,
-			"text"=> 'aa',
-			'mid' => $this->ami_mid
-			,'contentMetadata' => [
+			'toType'=> 1,
+			'text'=> 'aa',
+			'mid' => $this->ami_mid,
+			'contentMetadata' => [
 				'AT_RECV_MODE' => 2,
 				'STKVER' => 2,
 				'STKID' => 108,
@@ -22,7 +22,6 @@ class Bot extends Linebot {
 		// メッセージ送信
 		$this->send_message($this->ami_mid, $res_content);
 	}
-	
 }
 
 
